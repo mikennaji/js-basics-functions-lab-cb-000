e1e9f95d54e3de91cf4adfc4ded869cd53700f25
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-17 21:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (someValue) {
   //returns the number of blocks given a value
-  return 42 - someValue;
+  return  someValue-42;
 }
 
 
