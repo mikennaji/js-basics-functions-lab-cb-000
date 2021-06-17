4edb175228275e6f8a714f01e0548745a9dd6978
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-17 21:50:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,14 @@ function distanceTravelledInFeet(a,b){
 
 function calculatesFarePrice(start,destination){
   let distance = distanceTravelledInFeet(start,destination);
+  if (distance<= 400){
+    return 0;
+  }
   if (distance>=400 && distance<=2000){
-
+    return distance* 0.02;
   }
   else if(distance>=2000&& distance<=2000){
-
+    return 25;
   }
   else{
     return "cannot travel that far";
