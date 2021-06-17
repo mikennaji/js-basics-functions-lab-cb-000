28ffe563c611fff2bcf09a907a736c74a7e55044
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-17 21:37:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function distanceFromHqInBlocks (someValue) {
   //returns the number of blocks given a value
   return 42 - someValue;
 }
+
+
+function distanceFromHqInFeet (someValue) {
+  return distanceFromHqInBlocks(someValue)*264;
+  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  //the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
